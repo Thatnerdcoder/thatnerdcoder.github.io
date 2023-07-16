2f1fda3547c7b106f4b3ba39cf7f0d3c5acf9137
Thatnerdcoder <47266652+Thatnerdcoder@users.noreply.github.com>
2023-07-16 12:12:43 +0530

    Add files via upload

---
+++ default.js
@@ -0,0 +1,15 @@
+import { loadCultureFiles } from '../common/culture-loader';
+import { PdfViewer, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields } from '@syncfusion/ej2-pdfviewer';
+// tslint:disable-next-line:max-line-length
+PdfViewer.Inject(Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields);
+ 
+/**
+ * Default PdfViewer sample
+ */
+ 
+    loadCultureFiles();
+    let viewer: PdfViewer = new PdfViewer();
+    viewer.serviceUrl = 'https://ej2services.syncfusion.com/production/web-services/api/pdfviewer';
+    viewer.appendTo('#pdfViewer');
+    viewer.load('PDF_Succinctly.pdf', null);
